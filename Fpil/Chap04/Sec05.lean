edb20c44b78238430dc0934b47af6295ad678e06
rikitoro <yoshiaki@sendai-nct.ac.jp>
2023-12-04 21:10:57 +0900

    update Chap04 Sec05

--- Fpil/Chap04/Sec05.lean
+++ Fpil/Chap04/Sec05.lean
@@ -71,7 +71,7 @@ instance [BEq α] : BEq (BinTree α) where
 
 def hashBinTree [Hashable α] : BinTree α → UInt64
   | BinTree.leaf => 0
-  | BinTree.branch l x r =>
+  | BinTree.branch l _ r =>
     mixHash 1 (mixHash (hashBinTree l) (hashBinTree r))
 
 instance [Hashable α] : Hashable (BinTree α) where
@@ -92,3 +92,80 @@ deriving instance BEq for Pos
 
 deriving instance BEq, Hashable for Pos
 deriving instance BEq, Hashable, Repr for NonEmptyList
+
+-- # Appending
+
+-- class HAppend (α : Type) (β : Type) (γ : outParam Type) where
+--   hAppend : α → β → γ
+
+instance : Append (NonEmptyList α) where
+  append xs ys :=
+    { head := xs.head, tail := xs.tail ++ ys.head :: ys.tail }
+
+#eval idahoSpiders ++ idahoSpiders
+
+instance : HAppend (NonEmptyList α) (List α) (NonEmptyList α) where
+  hAppend xs ys :=
+    { head := xs.head, tail := xs.tail ++ ys }
+
+#eval idahoSpiders ++ ["Trapdoor Spider", "Hoge Spider"]
+
+
+-- # Functors
+
+#eval Functor.map (· + 5) [1, 2, 3]
+#eval (· + 5) <$> [1, 2, 3]
+
+#eval Functor.map toString (some (List.cons 5 List.nil))
+#eval toString <$> (some $ List.cons 5 List.nil)
+
+#eval Functor.map List.reverse [[1, 2, 3], [4, 5, 6]]
+#eval List.reverse <$> [[1, 2, 3], [4, 5, 6]]
+
+instance : Functor NonEmptyList where
+  map f xs := { head := f xs.head, tail := f <$> xs.tail }
+
+#eval (· ++ " kumo") <$> idahoSpiders
+
+instance : Functor PPoint where
+  map f p := { x := f p.x, y := f p.y }
+
+
+def concat [Append α] (xs : NonEmptyList α) : α :=
+  let rec catList (start : α) : List α → α
+    | [] => start
+    | z :: zs => catList (start ++ z) zs
+  catList xs.head xs.tail
+
+#eval concat idahoSpiders
+
+-- class Functor (f : Type → Type) where
+--   map : {α β : Type} → (α → β) → f α → f β
+
+--   mapConst {α β : Type} (x : α) (coll : f β) : f α :=
+--     map (fun _ => x) coll
+
+-- # Exercises
+
+def BinTree.map (f : α → β) : BinTree α → BinTree β
+  | BinTree.leaf => BinTree.leaf
+  | BinTree.branch l x r =>
+    BinTree.branch (BinTree.map f l) (f x) (BinTree.map f r)
+
+instance : Functor BinTree where
+  map := BinTree.map
+
+
+def BinTree.toString [ToString α] : BinTree α → String
+  | BinTree.leaf => "<Leaf>"
+  | BinTree.branch l x r =>
+    s!"({toString l}) " ++ s!"{x}" ++ s!" ({toString r})"
+
+instance : ToString (BinTree Pos) where
+  toString := BinTree.toString
+
+#eval bTree1
+#eval (· * (2 : Pos)) <$> bTree1
+
+#eval bTree3
+#eval (· * (2 : Pos)) <$> bTree3
