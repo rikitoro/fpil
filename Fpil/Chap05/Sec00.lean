d1f08aaf580b19ed2dc9f0a87de1a54ce88f47fd
rikitoro <yoshiaki@sendai-nct.ac.jp>
2023-12-05 22:39:43 +0900

    new Chap05

---
+++ Fpil/Chap05/Sec00.lean
@@ -0,0 +1,27 @@
+-- # Monads
+
+-- # Checking for none : Don't Repeat Yourself
+
+def first (xs : List α) : Option α :=
+  xs[0]?
+
+def firstThird (xs : List α) : Option (α × α) :=
+  match xs[0]? with
+  | none => none
+  | some first =>
+    match xs[2]? with
+    | none => none
+    | some third =>
+      some (first, third)
+
+def firstThirdFifth (xs : List α) : Option (α × α × α) :=
+  match xs[0]? with
+  | none => none
+  | some first =>
+    match xs[2]? with
+    | none => none
+    | some third =>
+      match xs[4]? with
+      | none => none
+      | some fifth =>
+        some (first, third, fifth)
