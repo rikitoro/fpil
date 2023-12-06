65b4d91053c9da915d8ba1eb9a4a5c27aae51454
rikitoro <yoshiaki@sendai-nct.ac.jp>
2023-12-06 22:08:11 +0900

    update Chap05 Sec02

--- Fpil/Chap05/Section02.lean
+++ Fpil/Chap05/Section02.lean
@@ -53,3 +53,79 @@ def evaluateOption : Expr Arith → Option Int
 
 #eval evaluateOption twoPlusThree
 #eval evaluateOption fourteenDivided
+
+def applyPrimE : Arith → Int → Int → Except String Int
+  | Arith.plus , x, y => pure (x + y)
+  | Arith.minus, x, y => pure (x - y)
+  | Arith.times, x, y => pure (x * y)
+  | Arith.div  , x, y =>
+    if y == 0 then
+      Except.error s!"Tried to divide {x} by zeor"
+    else pure (x / y)
+
+def evaluateExcept : Expr Arith → Except String Int
+  | Expr.const i => pure i
+  | Expr.prim p e1 e2 =>
+    evaluateExcept e1 >>= fun v1 =>
+    evaluateExcept e2 >>= fun v2 =>
+    applyPrimE p v1 v2
+
+#eval evaluateExcept twoPlusThree
+#eval evaluateExcept fourteenDivided
+
+
+def applyPrimOption : Arith → Int → Int → Option Int
+  | Arith.plus,  x, y => pure (x + y)
+  | Arith.minus, x, y => pure (x - y)
+  | Arith.times, x, y => pure (x * y)
+  | Arith.div,   x, y =>
+    if y == 0 then
+      none
+    else pure (x / y)
+
+def applyPrimExcept := applyPrimE
+
+def evaluateM [Monad m] (applyPrim : Arith → Int → Int → m Int) : Expr Arith → m Int
+  | Expr.const i => pure i
+  | Expr.prim p e1 e2 =>
+    evaluateM applyPrim e1 >>= fun v1 =>
+    evaluateM applyPrim e2 >>= fun v2 =>
+    applyPrim p v1 v2
+
+#eval evaluateM applyPrimOption twoPlusThree
+#eval evaluateM applyPrimOption fourteenDivided
+
+#eval evaluateM applyPrimExcept twoPlusThree
+#eval evaluateM applyPrimExcept fourteenDivided
+
+
+def applyDivOption (x : Int) (y : Int) : Option Int :=
+  if y == 0 then
+    none
+  else pure (x / y)
+
+def applyDivExcept (x : Int) (y : Int) : Except String Int :=
+  if y == 0 then
+    Except.error s!"Tried to divide {x} by zero"
+  else pure (x / y)
+
+def applyPrim' [Monad m] (applyDiv : Int → Int → m Int) : Arith → Int → Int → m Int
+  | Arith.plus,  x, y => pure (x + y)
+  | Arith.minus, x, y => pure (x - y)
+  | Arith.times, x, y => pure (x * y)
+  | Arith.div,   x, y => applyDiv x y
+
+def evaluateM' [Monad m] (applyDiv : Int → Int → m Int) : Expr Arith → m Int
+  | Expr.const i => pure i
+  | Expr.prim p e1 e2 =>
+    evaluateM' applyDiv e1 >>= fun v1 =>
+    evaluateM' applyDiv e2 >>= fun v2 =>
+    applyPrim' applyDiv p v1 v2
+
+#eval evaluateM' applyDivOption twoPlusThree
+#eval evaluateM' applyDivOption fourteenDivided
+
+#eval evaluateM' applyDivExcept twoPlusThree
+#eval evaluateM' applyDivExcept fourteenDivided
+
+-- # Further Effects
