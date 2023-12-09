536900ac343e095d8ce8780fda6732507f1997f9
rikitoro <yoshiaki@sendai-nct.ac.jp>
2023-12-09 18:59:26 +0900

    new Chap05 Sec03

---
+++ Fpil/Util/BinTree.lean
@@ -0,0 +1,11 @@
+inductive BinTree (α : Type) where
+  | leaf : BinTree α
+  | branch : BinTree α → α → BinTree α → BinTree α
+
+def eqBinTree [BEq α] : BinTree α → BinTree α → Bool
+  | BinTree.leaf, BinTree.leaf =>
+    true
+  | BinTree.branch l x r, BinTree.branch l2 x2 r2 =>
+    x == x2 && eqBinTree l l2 && eqBinTree r r2
+  | _, _ =>
+    false
