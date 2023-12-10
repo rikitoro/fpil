321b54d5ef0c33fce815e3e29401832c44e6cf02
rikitoro <yoshiaki@sendai-nct.ac.jp>
2023-12-10 21:02:44 +0900

    update Chap06 Sec02 Subtype

--- Fpil/Chap06/Sec02.lean
+++ Fpil/Chap06/Sec02.lean
@@ -13,3 +13,35 @@ instance : Applicative (Except ε) where
     match f with
     | .error e => .error e
     | .ok g => g <$> x ()
+
+structure Pair (α β : Type) : Type where
+  first : α
+  second : β
+
+instance : Functor (Pair α) where
+  map f x := ⟨x.first, f x.second⟩
+
+-- # User Input
+
+structure RawInput where
+  name : String
+  bitrhYear : String
+
+-- Subtypes
+
+#print Subtype
+-- structure Subtype {α : Type} (p : α → Prop) where
+--   val : α
+--   property : p val
+
+def FastPos : Type := {x : Nat // x > 0}
+
+def one : FastPos := ⟨1, by simp⟩
+
+instance : OfNat FastPos (n + 1) where
+  ofNat := ⟨n + 1, by simp_arith⟩
+
+def Nat.asFastPos? (n : Nat) : Option FastPos :=
+  if h : n > 0 then
+    some ⟨n, h⟩
+  else none
