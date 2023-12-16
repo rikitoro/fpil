3f8020385c21c0507c2b49e0c8f5589d07ef77a7
rikitoro <yoshiaki@sendai-nct.ac.jp>
2023-12-16 21:32:12 +0900

    update Chap07 Sec02

--- Fpil/Chap07/Sec02.lean
+++ Fpil/Chap07/Sec02.lean
@@ -64,3 +64,118 @@ instance [Monad m] : Alternative (OptionT m) where
 instance [Monad m] : MonadLift m (OptionT m) where
   monadLift action := OptionT.mk do
     pure $ some $ ← action
+
+-- # Exceptions
+
+-- def ExceptT (ε: Type u) (m : Type u → Type v) (α : Type u) : Type v :=
+--   m (Except ε α)
+
+-- def ExceptT.mk {ε α : Type u} (x : m (Except ε α)) : ExceptT ε m α := x
+-- def ExceptT.run {ε α : Type u} (x : ExceptT ε m α) : m (Except ε α) := x
+
+instance {ε : Type u} {m : Type u → Type v} [Monad m] : Monad (ExceptT ε m) where
+  pure x := ExceptT.mk $ pure $ Except.ok x
+  bind result next := ExceptT.mk do
+    match ← result with
+    | .error e => pure $ .error e
+    | .ok x => next x
+
+instance [Monad m] : MonadLift (Except ε) (ExceptT ε m) where
+  monadLift action := ExceptT.mk (pure action)
+
+instance [Monad m] : MonadLift m (ExceptT ε m) where
+  monadLift action := ExceptT.mk (.ok <$> action)
+
+-- # Type Classes for Exceptions
+
+-- class MonadExcept (ε : outParam (Type u)) (m : Type v → Type w) where
+--   throw : ε → m α
+--   tryCatch : m α → (ε → m α) → m α
+
+-- inductive Err where
+--   | divByZero
+--   | notANumber : String → Err
+
+inductive Err where
+  | divByZero
+  | notANumber : String → Err
+
+def divBackend [Monad m] [MonadExcept Err m] (n k : Int) : m Int :=
+  if k == 0 then
+    throw .divByZero
+  else pure (n / k)
+
+def asNumber [Monad m] [MonadExcept Err m] (s : String) : m Int :=
+  match s.toInt? with
+  | none => throw (.notANumber s)
+  | some i => pure i
+
+def divFrontend [Monad m] [MonadExcept Err m] (n k : String) : m String :=
+  tryCatch (do pure $ toString (← divBackend (← asNumber n) (← asNumber k)))
+    fun
+    | .divByZero => pure "Division By zero!"
+    | .notANumber s => pure s!"Not a number: \"{s}\""
+
+def divFrontend' [Monad m] [MonadExcept Err m] (n k : String) : m String :=
+  try
+    pure $ toString $ ← divBackend (← asNumber n) (← asNumber k)
+  catch
+    | .divByZero => pure "Division by zero!"
+    | .notANumber s => pure s!"Not a number: \"{s}\""
+
+
+-- # State
+
+-- def StateT (σ : Type u) (m : Type u → Type v) (α : Type u) : Type (max u v) :=
+--   σ → m (α × σ)
+
+instance [Monad m] : Monad (StateT σ m) where
+  pure x := fun s => pure (x, s)
+  bind result next := fun s => do
+    let (v, s') ← result s
+    next v s'
+
+structure LetterCounts where
+  vowels : Nat
+  consonants : Nat
+deriving Repr
+
+inductive Err' where
+  | notALetter : Char → Err'
+deriving Repr
+
+def vowels :=
+  let lowerVowels := "aeiuoy"
+  lowerVowels ++ lowerVowels.map (·.toUpper)
+
+def consonants :=
+  let lowerConsonants := "bcdfghjklmnpqrstvwxz"
+  lowerConsonants ++ lowerConsonants.map (·.toUpper)
+
+def countLettters (str : String) : StateT LetterCounts (Except Err') Unit :=
+  let rec loop (chars : List Char) := do
+    match chars with
+    | [] => pure ()
+    | c :: cs =>
+      let st ← get
+      let st' ←
+        if c.isAlpha then
+          if vowels.contains c then
+            pure {st with vowels := st.vowels + 1}
+          else if consonants.contains c then
+            pure {st with consonants := st.consonants + 1}
+          else
+            pure st
+        else throw (.notALetter c)
+      set st'
+      loop cs
+  loop str.toList
+
+
+-- class MonadState (σ : outParam (Type u)) (m : Type u → Type v) : Type (max (u + 1) w) where
+--   get : m σ
+--   set : σ → m PUnit
+--   modifyGet : (σ → α × σ) → m α
+
+-- def modify [MonadState σ m] (f : σ → σ) : m Unit :=
+--   modifyGer fun s => ((), f s)
