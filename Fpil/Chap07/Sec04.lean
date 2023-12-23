6b3658114aad080c9382463a41654140e0ab559f
rikitoro <yoshiaki@sendai-nct.ac.jp>
2023-12-23 21:00:51 +0900

    update Chap07 Sec04 ForM

--- Fpil/Chap07/Sec04.lean
+++ Fpil/Chap07/Sec04.lean
@@ -65,3 +65,47 @@ def greet (name : String) : String :=
   "Hello, " ++ Id.run do return name
 
 #eval greet "David"
+
+-- # Loops
+
+-- # Looping with ForM
+
+-- class ForM (m : Type u → Type v) (γ : Type w₁) (α : outParam (Type w₂)) where
+--   forM [Monad m] : γ → (α → m PUnit) → m PUnit
+
+#print PUnit
+-- PUnit is a version of the Unit type that is universe-polymorphic to allow it to be in Type u instead of Type.
+
+-- def List.forM [Monad m] : List α → (α → m PUnit) → m PUnit
+--   | [], _ => pure ()
+--   | x :: xs, action => do
+--     action x
+--     forM xs action
+
+-- instance : ForM m (List α) α where
+--   forM := List.forM
+
+def countLetters' (str : String) : StateT LetterCounts (Except Err) Unit :=
+  forM str.toList fun c => do
+    if c.isAlpha then
+      if vowels.contains c then
+        modify fun st => {st with vowels := st.vowels + 1}
+      else if consonants.contains c then
+        modify fun st => {st with consonants := st.consonants + 1}
+    else throw (.notALetter c)
+
+structure AllLessThan where
+  num : Nat
+
+def AllLessThan.forM [Monad m] (coll : AllLessThan) (action : Nat → m Unit) : m Unit :=
+  let rec countdown : Nat → m Unit
+    | 0 => pure ()
+    | n + 1 => do
+      action n
+      countdown n
+  countdown coll.num
+
+instance : ForM m AllLessThan Nat where
+  forM := AllLessThan.forM
+
+#eval forM { num := 5 : AllLessThan} IO.println
