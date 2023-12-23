c71ce5d1466c3ac7c1c44b00639e2220ba9c8f38
rikitoro <yoshiaki@sendai-nct.ac.jp>
2023-12-24 08:36:57 +0900

    update Chap07 Sec05 Infinite Loops

--- Fpil/Chap07/Sec05.lean
+++ Fpil/Chap07/Sec05.lean
@@ -17,3 +17,28 @@ def times3 (n : Nat) : Nat := n * 3
 
 #eval ([1, 2, 3].reverse.drop 1).reverse
 #eval [1, 2, 3] |> List.reverse |> List.drop 1 |> List.reverse
+
+-- # Infinite Loops
+
+def spam : IO Unit := do
+  repeat IO.println "Spam!"
+
+-- #eval spam
+
+def bufsize : USize := 20 * 1024
+
+def dump (stream : IO.FS.Stream) : IO Unit := do
+  let stdout ← IO.getStdout
+  repeat do
+    let buf ← stream.read bufsize
+    if buf.isEmpty then break
+    stdout.write buf
+
+-- # While Loops
+
+def dump'(stream : IO.FS.Stream) : IO Unit := do
+  let stdout ← IO.getStdout
+  let mut buf ← stream.read bufsize
+  while not buf.isEmpty do
+    stdout.write buf
+    buf ← stream.read bufsize
