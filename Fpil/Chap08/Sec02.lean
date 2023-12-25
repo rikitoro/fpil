6a61c52f3b5b6ea85f9b2c96bb72c3cbd433b7f8
rikitoro <yoshiaki@sendai-nct.ac.jp>
2023-12-26 07:12:06 +0900

    update Chap08 Sec02

--- Fpil/Chap08/Sec02.lean
+++ Fpil/Chap08/Sec02.lean
@@ -58,3 +58,33 @@ abbrev Finite.asType : Finite → Type
   | .bool => Bool
   | .pair t1 t2 => asType t1 × asType t2
   | .arr t1 t2 => asType t1 → asType t2
+
+def List.product (xs : List α) (ys : List β) : List (α × β) := Id.run do
+  let mut out : List (α × β) := []
+  for x in xs do
+    for y in ys do
+      out := (x, y) :: out
+  pure out.reverse
+
+def Finite.functions (t : Finite) (results : List α) : List (t.asType → α) :=
+  match t with
+  | .unit =>
+    results.map fun r =>
+      fun () => r
+  | .bool -- !!!implement here!!!
+
+def Finite.enuerate (t : Finite) : List t.asType :=
+  | .unit => [()]
+  | .bool => [true, false]
+  | .pair t1 t2 => t1.enumerate.product t2.enumerate
+  | .arr t1 t2 => t1.functions t2.enumerate
+
+def Finite.beq (t : Finite) (x y : t.asType) : Bool :=
+  match t with
+  | .unit => true
+  | .bool => x == y
+  | .pair t1 t2 => beq t1 x.fst y.fst && beq t2 x.snd y.snd
+  | .arr t1 t2 =>
+    t1.enumerate.all fun arg => beq t2 (x arg) (y arg) -- すべての引数 arg に対して x arg == y arg
+
+#eval Finite.beq (.arr .bool .bool) (fun _ => true) (fun b => b == b)
