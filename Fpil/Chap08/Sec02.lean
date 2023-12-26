4e105a976e541bbae6dc02c23b6dc24370230869
rikitoro <yoshiaki@sendai-nct.ac.jp>
2023-12-26 21:08:47 +0900

    update Chap08 Sec02

--- Fpil/Chap08/Sec02.lean
+++ Fpil/Chap08/Sec02.lean
@@ -66,18 +66,46 @@ def List.product (xs : List α) (ys : List β) : List (α × β) := Id.run do
       out := (x, y) :: out
   pure out.reverse
 
-def Finite.functions (t : Finite) (results : List α) : List (t.asType → α) :=
-  match t with
-  | .unit =>
-    results.map fun r =>
-      fun () => r
-  | .bool -- !!!implement here!!!
+-- def List.foldr (f : α → β → α) (default : β) : List α → β
+--   | []  => default
+--   | a :: l => f a <| foldr f default l
 
-def Finite.enuerate (t : Finite) : List t.asType :=
-  | .unit => [()]
-  | .bool => [true, false]
-  | .pair t1 t2 => t1.enumerate.product t2.enumerate
-  | .arr t1 t2 => t1.functions t2.enumerate
+#eval [1, 2, 3, 4, 5].foldr (· + ·) 0
+
+mutual
+  def Finite.functions (t : Finite) (results : List α) : List (t.asType → α) :=
+    match t with
+    | .unit =>
+      results.map fun r =>
+        fun () => r
+    | .bool =>
+      (results.product results).map fun (r1, r2) =>
+        fun
+          | true  => r1
+          | false => r2
+    | .pair t1 t2 =>
+      let fls := t1.functions <| t2.functions results
+      fls.map fun f =>
+        fun (x, y) => f x y
+    | .arr t1 t2 =>
+      let args := t1.enumerate
+      let base :=
+        results.map fun r =>
+          fun _ => r
+      args.foldr (
+        fun arg rest =>
+          (t2.functions rest).map fun more =>
+            fun f => more (f arg) f)
+      base
+
+
+  def Finite.enumerate (t : Finite) : List t.asType :=
+    match t with
+    | .unit => [()]
+    | .bool => [true, false]
+    | .pair t1 t2 => t1.enumerate.product t2.enumerate
+    | .arr t1 t2 => t1.functions t2.enumerate
+end
 
 def Finite.beq (t : Finite) (x y : t.asType) : Bool :=
   match t with
