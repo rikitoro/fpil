fcfcd81588ea2d06c87f104c6bc6798d80fe846d
rikitoro <yoshiaki@sendai-nct.ac.jp>
2023-12-28 15:47:20 +0900

    update Chap08 Sec03

--- Fpil/Chap08/Sec03.lean
+++ Fpil/Chap08/Sec03.lean
@@ -170,3 +170,146 @@ def Row.project (row : Row s) : (s' : Schema) → Subschema s' s → Row s'
   | [], .nil => ()
   | [_], .cons c .nil => row.get c
   | _::_::_, .cons c cs => (row.get c, row.project _ cs)
+
+-- # conditions and selection
+
+inductive DBExpr (s : Schema) : DBType → Type where
+  | col (n : String) (loc : HasCol s n t) : DBExpr s t
+  | eq (e1 e2 : DBExpr s t) : DBExpr s .bool
+  | lt (e1 e2 : DBExpr s .int) : DBExpr s .bool
+  | and (e1 e2 : DBExpr s .bool) : DBExpr s .bool
+  | const : t.asType → DBExpr s t
+
+
+def tallInDenmark : DBExpr peak .bool :=
+  .and
+    (.lt (.const 1000) (.col "elevation" (by repeat constructor)))
+    (.eq (.col "location" (by repeat constructor)) (.const "Denmark"))
+
+macro "c!" n:term : term => `(DBExpr.col $n (by repeat constructor))
+
+def tallInDenmark' : DBExpr peak .bool :=
+  .and
+    (.lt (.const 1000) (c! "elevation"))
+    (.eq (c! "location") (.const "Denmark"))
+
+def DBExpr.evaluate (row : Row s) : DBExpr s t → t.asType
+  | .col _ loc  => row.get loc
+  | .eq e1 e2   => evaluate row e1 == evaluate row e2
+  | .lt e1 e2   => evaluate row e1 < evaluate row e2
+  | .and e1 e2  => evaluate row e1 && evaluate row e2
+  | .const v => v
+
+#eval tallInDenmark.evaluate ("Valby Bakke", "Denmark", 31, 2023)
+#eval tallInDenmark.evaluate ("Fictional mountain", "Denmark", 1230, 2011)
+#eval tallInDenmark.evaluate ("Mount Borah", "USA", 3859, 1996)
+
+-- # Queries
+
+def disjoint [BEq α] (xs ys : List α) : Bool :=
+  not (xs.any ys.contains || ys.any xs.contains)
+
+def Schema.renameColumn : (s : Schema) → HasCol s n t → String → Schema
+  | c :: cs, .here, n' => {c with name := n'} :: cs
+  | c :: cs, .there next, n' => c :: renameColumn cs next n'
+
+
+inductive Query : Schema → Type where
+  | table   : Table s → Query s
+  | union   : Query s → Query s → Query s
+  | diff    : Query s → Query s → Query s
+  | select  : Query s → DBExpr s .bool → Query s
+  | project : Query s → (s' : Schema) → Subschema s' s → Query s'
+  | product :
+    Query s1 → Query s2 →
+    disjoint (s1.map Column.name) (s2.map Column.name) →
+    Query (s1 ++ s2)
+  | renameColumn :
+    Query s → (c : HasCol s n t) → (n' : String) → !((s.map Column.name).contains n') →
+    Query (s.renameColumn c n')
+  | prefixWith :
+    (n : String) → Query s →
+    Query (s.map fun c => {c with name := n ++ "." ++ c.name})
+
+
+-- # Executing Queries
+
+-- ## Cartesian Product
+
+def addVal (v : c.contains.asType) (row : Row s) : Row (c :: s) :=
+  match s, row with
+    | [], () => v
+    | c' :: cs, v' => (v, v')
+
+def Row.append (r1 : Row s1) (r2 : Row s2) : Row (s1 ++ s2) :=
+  match s1, r1 with
+  | [], () => r2
+  | [_], v => addVal v r2
+  | _::_::_, (v, r') => (v, r'.append r2)
+
+def List.flatMap (f : α → List β) : (xs : List α) → List β
+  | [] => []
+  | x :: xs => f x ++ xs.flatMap f
+
+def Table.cartesianProduct (table1 : Table s1) (table2 : Table s2) : Table (s1 ++ s2) :=
+  table1.flatMap fun r1 => table2.map r1.append
+
+def Table.cartesianProduct' (table1 : Table s1) (table2 : Table s2) : Table (s1 ++ s2) := Id.run do
+  let mut out : Table (s1 ++ s2) := []
+  for r1 in table1 do
+    for r2 in table2 do
+      out := (r1.append r2) :: out
+  pure out.reverse
+
+-- ## Differece
+
+#eval ["Willamette", "Columbia", "Sandy", "Deschutes"].filter (·.length > 8)
+
+def List.without [BEq α] (source banned : List α) : List α :=
+  source.filter fun r => !(banned.contains r)
+
+-- ## Renaming Columns
+
+def Row.rename (c : HasCol s n t) (row : Row s) : Row (s.renameColumn c n') :=
+  match s, row, c with
+  | [_], v, .here => v
+  | _::_::_, (v, r), .here => (v, r)
+  | _::_::_, (v, r), .there next => addVal v (r.rename next)
+
+-- ## Prefixing Column Names
+
+def prefixRow (row : Row s) : Row (s.map fun c => {c with name := n ++ "." ++ c.name}) :=
+  match s, row with
+  | [], _ => ()
+  | [_], v => v
+  | _::_::_, (v, r) => (v, prefixRow r)
+
+-- ## Putting the Pieces Together
+
+def Query.exec : Query s → Table s
+  | .table t => t
+  | .union q1 q2 => exec q1 ++ exec q2
+  | .diff q1 q2 => exec q1 |>.without (exec q2)
+  | .select q e => exec q |>.filter e.evaluate
+  | .project q _ sub => exec q |>.map (·.project _ sub)
+  | .product q1 q2 _ => exec q1 |>.cartesianProduct (exec q2)
+  | .renameColumn q c _ _ => exec q |>.map (·.rename c)
+  | .prefixWith _ q => exec q |>.map prefixRow
+
+open Query in
+def example1 :=
+  table mountainDiary |>.select
+  (.lt (.const 500) (c! "elevation")) |>.project
+  [⟨"elevation", .int⟩] (by repeat constructor)
+
+#eval example1.exec
+
+open Query in
+def example2 :=
+  let mountain := table mountainDiary |>.prefixWith "mountain"
+  let waterfall := table waterfallDiary |>.prefixWith "waterfall"
+  mountain.product waterfall (by simp)
+    |>.select (.eq (c! "mountain.location") (c! "waterfall.location"))
+    |>.project [⟨"mountain.name", .string⟩, ⟨"waterfall.name", .string⟩] (by repeat constructor)
+
+#eval example2.exec
