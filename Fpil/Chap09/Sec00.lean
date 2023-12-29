b02eae5cc31b30355ef37acf7f5f716d86f0c78d
rikitoro <yoshiaki@sendai-nct.ac.jp>
2023-12-29 16:55:44 +0900

    update Chap09 Sec00 Tactic Golf

--- Fpil/Chap09/Sec00.lean
+++ Fpil/Chap09/Sec00.lean
@@ -1,4 +1,5 @@
 -- # Interlude : Tactics, Induction, and Proofs
+import Fpil.Util.BinTree
 
 def Nat.plusL : Nat → Nat → Nat
   | 0, k => k
@@ -16,3 +17,24 @@ theorem plusR_zero_left (k : Nat) : k = Nat.plusR 0 k := by
   | succ n ih =>
     unfold Nat.plusR
     rw [←ih]
+
+-- # Tactic Golf
+
+theorem plusR_zero_left' (k : Nat) : k = Nat.plusR 0 k := by
+  induction k with
+  | zero => rfl
+  | succ n ih =>
+    simp [Nat.plusR]
+    assumption
+
+theorem plusR_zero_left'' (k : Nat) : k = Nat.plusR 0 k := by
+  induction k
+  case zero => simp [Nat.plusR]
+  case succ n ih =>
+    simp [Nat.plusR]
+    assumption
+
+theorem plusR_zero_left''' (k : Nat) : k = Nat.plusR 0 k := by
+  induction k <;> simp [Nat.plusR] <;> assumption
+
+-- # Induction on Other Datatypes
