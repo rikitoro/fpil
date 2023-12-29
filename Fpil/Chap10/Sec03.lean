a82a94a75a5cd9a5c21fd66046bbbf5916f7525c
rikitoro <yoshiaki@sendai-nct.ac.jp>
2023-12-30 07:49:43 +0900

    update Chap10 Sec03 Proving Termination

--- Fpil/Chap10/Sec03.lean
+++ Fpil/Chap10/Sec03.lean
@@ -58,3 +58,30 @@ instance : LT Nat where
 theorem four_lt_seven : 4 < 7 :=
   open Nat.le in
   step $ step refl
+
+-- # Proving Termination
+def arrayMapHelper (f : α → β) (arr : Array α) (soFar : Array β) (i : Nat) : Array β :=
+  if inBounds : i < arr.size then
+    arrayMapHelper f arr (soFar.push $ f arr[i]) (i + 1)
+  else soFar
+termination_by arrayMapHelper _ arr _ i => arr.size - i
+
+def  Array.map' (f : α → β) (arr : Array α) : Array β :=
+  arrayMapHelper f arr Array.empty 0
+
+#eval Array.map' (fun str => str.length) #["abc", "de", "f", ""]
+
+def findHelper (arr : Array α) (p : α → Bool) (i : Nat) : Option (Nat × α) :=
+  if h : i < arr.size then
+    let x := arr[i]
+    if  p x then
+      some (i, x)
+    else findHelper arr p (i + 1)
+  else none
+termination_by findHelper arr _ i => arr.size - i
+
+
+def Array.find' (arr : Array α) (p : α → Bool) : Option (Nat × α) :=
+  findHelper arr p 0
+
+#eval (#["abc", "de", "f", ""]).find' (fun str => str.length == 2)
